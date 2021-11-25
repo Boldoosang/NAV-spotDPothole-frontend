0521d0e520b05c2dec2fcb707ff1f27dcf6c75c6
Boldoosang <jbaldeo@yahoo.com>
2021-11-25 05:33:24 -0400

    Added report canvas

--- public/index.js
+++ public/index.js
@@ -134,3 +134,44 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+
+async function loadReports(potholeID){
+    potholeID = 87
+    let potholeReports = await sendRequest(SERVER + "/api/reports/pothole/" + potholeID, "GET")
+    let allReportsContainer = document.querySelector("#reportAccordion")
+
+    let allReportsAccordions = ""
+    
+    try {
+        potholeReports = potholeReports.sort((a, b) => {
+            if (a.potholeID < b.potholeID)
+                return 1;                
+            else
+                return -1;
+        })
+
+        for(report of potholeReports){
+            console.log(report)
+            allReportsAccordions += 
+            `<div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    ReportID: ${report.reportID} reported by <Insert Name of Reporter>
+                </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    </div>
+                </div>
+            </div>`
+        }
+    } catch(e){
+        allReportsAccordions = `<p>No reports for this pothole!</p>`
+    }
+
+    allReportsContainer.innerHTML = allReportsAccordions
+}
+
+loadReports(1)
+
