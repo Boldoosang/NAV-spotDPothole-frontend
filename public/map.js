b04714691405741e2f15e6305743373a5e97b0ec
Boldoosang <jbaldeo@yahoo.com>
2021-11-29 14:44:28 -0400

    Compressed geojson and logo files.

--- public/map.js
+++ public/map.js
@@ -30,173 +30,173 @@ if(container) {
 
     displayPotholes()
 }
-    function getRandom(){
-        return today.getSeconds()/today.getMinutes() * 0.05
-    }
+function getRandom(){
+    return today.getSeconds()/today.getMinutes() * 0.05
+}
 
-    async function submitReport(){
-        today = new Date();
-            let randLat = Math.random() * getRandom();
-            let randLong = Math.random() * getRandom();
+async function submitReport(){
+    today = new Date();
+        let randLat = Math.random() * getRandom();
+        let randLong = Math.random() * getRandom();
 
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(async function(position) {
-                const constituency = leafletPip.pointInLayer([position.coords.longitude, position.coords.latitude], map);
-                    if(constituency.length == 0)
-                        return
-                    const data = {
-                        "longitude" : position.coords.longitude,
-                        "latitude" : position.coords.latitude,
-                        "constituencyID" : constituency[0].feature.properties.Constituency,
-                    }
-
-                    let result =  await sendRequest(SERVER + '/api/reports/driver', 'POST', data);
-                    if(markersLayer)
-                        markersLayer.clearLayers();
-
-                    displayPotholes();
-                });     
-            }else {
-                alert("Geolocation is not supported by this browser.");
-                return null;
-            }
-    }
-
-    async function submitReportClickTest(lat, long){
-        const constituency = leafletPip.pointInLayer([long, lat], map);
-
-        const data = {
-            "longitude" : long,
-            "latitude" : lat,
-            "constituencyID" : constituency[0].feature.properties.Constituency,
-        }
-
-        let result =  await sendRequest(SERVER + '/api/reports/driver', 'POST', data);
-        
-        if(markersLayer)
-            markersLayer.clearLayers();
-
-        displayPotholes();
-    }
-
-    async function getPotholes(){
-        let potholes = await sendRequest(SERVER + '/api/potholes', 'GET');
-        return potholes;
-    }
-
-    async function displayPotholes(){
-        let potholes = await getPotholes();
-        if(potholes.length > 0){
-            for(const pothole of potholes){
-                var constituency = leafletPip.pointInLayer([pothole.longitude, pothole.latitude], map);
-
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async function(position) {
+            const constituency = leafletPip.pointInLayer([position.coords.longitude, position.coords.latitude], map);
                 if(constituency.length == 0)
                     return
+                const data = {
+                    "longitude" : position.coords.longitude,
+                    "latitude" : position.coords.latitude,
+                    "constituencyID" : constituency[0].feature.properties.Constituency,
+                }
+
+                let result =  await sendRequest(SERVER + '/api/reports/driver', 'POST', data);
+                if(markersLayer)
+                    markersLayer.clearLayers();
+
+                displayPotholes();
+            });     
+        }else {
+            alert("Geolocation is not supported by this browser.");
+            return null;
+        }
+}
+
+async function submitReportClickTest(lat, long){
+    const constituency = leafletPip.pointInLayer([long, lat], map);
+
+    const data = {
+        "longitude" : long,
+        "latitude" : lat,
+        "constituencyID" : constituency[0].feature.properties.Constituency,
+    }
+
+    let result =  await sendRequest(SERVER + '/api/reports/driver', 'POST', data);
     
-                if(constituency.length == 0){
-                    constituency = [
-                        {
-                            "feature": {
-                                "properties": {
-                                    "ID": "null",
-                                }
+    if(markersLayer)
+        markersLayer.clearLayers();
+
+    displayPotholes();
+}
+
+async function getPotholes(){
+    let potholes = await sendRequest(SERVER + '/api/potholes', 'GET');
+    return potholes;
+}
+
+async function displayPotholes(){
+    let potholes = await getPotholes();
+    if(potholes.length > 0){
+        for(const pothole of potholes){
+            var constituency = leafletPip.pointInLayer([pothole.longitude, pothole.latitude], map);
+
+            if(constituency.length == 0)
+                return
+
+            if(constituency.length == 0){
+                constituency = [
+                    {
+                        "feature": {
+                            "properties": {
+                                "ID": "null",
                             }
                         }
-                    ]
-                }
-                
-                let marker = L.marker([pothole.latitude, pothole.longitude], {
-                    constituency: constituency[0].feature.properties.Constituency,
-                    potholeID: pothole.potholeID,
-                    constituencyID: constituency[0].feature.properties.ID
-                }).on('click', async function(){
-                    var constituency = leafletPip.pointInLayer([pothole.longitude, pothole.latitude], map);
-                    var constituencyName = document.getElementById('constituencyName')
-                    constituencyName.innerText = constituency[0].feature.properties.Constituency;
-    
-                    loadReports(this.options.potholeID);
-                    loadConstituencyData(this.options.constituencyID)
-                    
-                    var offCanvasReport= getOffCanvas();
-                    offCanvasReport.toggle();
-                }).bindPopup(pothole.numReports + " Report(s)");
-    
-                markers.push(marker)
-                markersLayer = L.layerGroup(markers); 
-                markersLayer.addTo(map);
-            }   
-        }  
-    }
-
-    function getOffCanvas(){
-        var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
-        var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
-            return new bootstrap.Offcanvas(offcanvasEl)
-        })
-
-        for(const offCanvas of offcanvasList){
-            if(offCanvas._element.id === 'offcanvasReport'){
-                return offCanvas;
+                    }
+                ]
             }
-        }
-    }
-
-    async function getPotholesByConstituency(){
-        let constituencies = []
-        
-        var leaderboardData = []
-        
-        for(var pothole of markers){
-            leaderboardData.push(pothole.options.constituency)
-        }
-
-        for(const pothole of markers){
-            let constituency = pothole.options.constituency;
-            let constituencyCode = pothole.options.constituencyID;
-
-            let found = false;
-            for(const c of constituencies){
-                if(c.name === constituency){
-                    c.count++;
-                    found = true;
-                }
-            }
-            if(!found){
-                //  let url = `${PICONG_SERVER}?year=${ELECTION_YEAR}&district=${pothole.options.constituencyID}`
-                //  leader = await sendRequest(url, "GET")
-
-                constituencies.push({
-                    name: constituency,
-                    count: 1,
-                    //constituencyLeader: leader[0].name
-                    constituencyLeader: "Filler data till we get more info from Picong Party"
-                })
-            }
-        }
-
-        constituencies.sort(function(a, b){
-            return b.count - a.count;
-        })
-
-        getReportLeaderboardData()
-        return constituencies;
-    }
-
-    async function getReportLeaderboardData(){
-        leaderboardData = []
-        let result = await sendRequest(SERVER + '/api/potholes', 'GET');
-
-        //sort result by numReports
-        result.sort(function(a, b){
-            return b.numReports - a.numReports;
-        })
-
-        for(const pothole of result){
-            var constituency = leafletPip.pointInLayer([pothole.longitude, pothole.latitude], map);
             
-            leaderboardData.push({
+            let marker = L.marker([pothole.latitude, pothole.longitude], {
                 constituency: constituency[0].feature.properties.Constituency,
+                potholeID: pothole.potholeID,
+                constituencyID: constituency[0].feature.properties.ID
+            }).on('click', async function(){
+                var constituency = leafletPip.pointInLayer([pothole.longitude, pothole.latitude], map);
+                var constituencyName = document.getElementById('constituencyName')
+                constituencyName.innerText = constituency[0].feature.properties.Constituency;
 
+                loadReports(this.options.potholeID);
+                loadConstituencyData(this.options.constituencyID)
+                
+                var offCanvasReport= getOffCanvas();
+                offCanvasReport.toggle();
+            }).bindPopup(pothole.numReports + " Report(s)");
+
+            markers.push(marker)
+            markersLayer = L.layerGroup(markers); 
+            markersLayer.addTo(map);
+        }   
+    }  
+}
+
+function getOffCanvas(){
+    var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+    var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+        return new bootstrap.Offcanvas(offcanvasEl)
+    })
+
+    for(const offCanvas of offcanvasList){
+        if(offCanvas._element.id === 'offcanvasReport'){
+            return offCanvas;
+        }
+    }
+}
+
+async function getPotholesByConstituency(){
+    let constituencies = []
+    
+    var leaderboardData = []
+    
+    for(var pothole of markers){
+        leaderboardData.push(pothole.options.constituency)
+    }
+
+    for(const pothole of markers){
+        let constituency = pothole.options.constituency;
+        let constituencyCode = pothole.options.constituencyID;
+
+        let found = false;
+        for(const c of constituencies){
+            if(c.name === constituency){
+                c.count++;
+                found = true;
+            }
+        }
+        if(!found){
+            //  let url = `${PICONG_SERVER}?year=${ELECTION_YEAR}&district=${pothole.options.constituencyID}`
+            //  leader = await sendRequest(url, "GET")
+
+            constituencies.push({
+                name: constituency,
+                count: 1,
+                //constituencyLeader: leader[0].name
+                constituencyLeader: "Filler data till we get more info from Picong Party"
             })
         }
     }
+
+    constituencies.sort(function(a, b){
+        return b.count - a.count;
+    })
+
+    getReportLeaderboardData()
+    return constituencies;
+}
+
+async function getReportLeaderboardData(){
+    leaderboardData = []
+    let result = await sendRequest(SERVER + '/api/potholes', 'GET');
+
+    //sort result by numReports
+    result.sort(function(a, b){
+        return b.numReports - a.numReports;
+    })
+
+    for(const pothole of result){
+        var constituency = leafletPip.pointInLayer([pothole.longitude, pothole.latitude], map);
+        
+        leaderboardData.push({
+            constituency: constituency[0].feature.properties.Constituency,
+
+        })
+    }
+}
