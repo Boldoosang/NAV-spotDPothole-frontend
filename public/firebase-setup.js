984190a34a2d127d45de1682c1309a8b9e2810f3
Boldoosang <jbaldeo@yahoo.com>
2021-12-02 05:02:33 -0400

    Added report request builder and error messages to report outcomes

--- public/firebase-setup.js
+++ public/firebase-setup.js
@@ -6,268 +6,211 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-
 
 import { firebaseConfig } from "./credentials.js";
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 
 // Get a reference to the storage service, which is used to create references in your storage bucket
 const myStorage = getStorage(app);
 
 
+//Backend Constants
+const SERVER = "https://spotdpothole.herokuapp.com"
+const driverReportURL = SERVER + "/api/report/driver"
+const standardReportURL = SERVER + "/api/report/standard"
+
+
 /* To display Toast */
 function displayToast(type, message) {
-  var id= new Date() + '-' + "notification"
-  var div = document.createElement('div');
-  div.textContent = message;
+	var id= new Date() + '-' + "notification"
+	var div = document.createElement('div');
+	div.textContent = message;
 
-  div.setAttribute('id', id)
-  div.setAttribute('class', '');
+	div.setAttribute('id', id)
+	div.setAttribute('class', '');
   
-  if( type=='success'){
-      div.setAttribute('class', "messsage success show");    
-  }
-  else{
-      div.setAttribute('class', "message failed show");
-  }
+	if( type=='success'){
+		div.setAttribute('class', "messsage success show");    
+	} else {
+		div.setAttribute('class', "message failed show");
+	}
   
-  
-  document.getElementById('mainTabContent').appendChild(div);
-  // After 3 seconds, remove the show class from DIV
-  setTimeout(() => { 
-    console.log(id)
-    let element = document.getElementById(id);
-    element.className = element.className.replace("show", "hide"); }, 3000);
+  	document.getElementById('mainTabContent').appendChild(div);
+	// After 3 seconds, remove the show class from DIV
+	setTimeout(() => { 
+		console.log(id)
+		let element = document.getElementById(id);
+		element.className = element.className.replace("show", "hide"); 
+	}, 3000);
 }
-
-
-
 
 
 //To send to the backend
 async function postDriverReport() {
+  	let results = await makeRequest(null, null, driverReportURL)
 
-  const backendurl = "http://spotdpothole.herokuapp.com" + "/api/reports/driver"
-  
-    //call method to upload only descripiton: makeRequest( photoURL, BackendURL)
-    var latitude;
-    var longitude;
-    //STEP2: get location
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        //Successful action
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-  
-  
-      }, alert("To obtain your location, permission must be granted first."))
-      
-
-      if( longitude==null || latitude==null ){
-        //add display message here
-        displayToast("failed", "unfortunately we couldn't find your coordinates!")
-        return;
-      }
-      
-      const data = {
-        "longitude": longitude,
-        "latitude": latitude
-      };
-  
-      console.log('data:' + JSON.stringify(data) );
-      try {
-        let access_token = window.localStorage.getItem("access_token");
-       
-        let request = {
-          "method": "POST",
-          "headers": {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${access_token}`,
-            
-          },
-          "body":JSON.stringify(data)
-        }
-        
-  
-  
-        
-        let response = await fetch(backendurl, request);
-        let results = await response.json()
-        //console.log(results)
-        if( "message" in results && ("Successfully" in results["message"] || "updated" in results["message"] )){
-          //add display message here
-          displayToast("success", results["message"])
-        }
-        else if(results){
-          displayToast("failed", results["message"])
-        }
-      }
-      catch (error) {
-        console.log(`Error: ` + error)
-        
-      }
-  
-    }//end of if
- 
     let driverReportOutcome = document.querySelector("#driverReportOutcome")
 
-    if("msg" in results){
-      driverReportOutcome.innerHTML = "<div class='text-danger'>You must be logged in to report a pothole!</div>"
-    } else if("error" in results){
-      driverReportOutcome.innerHTML = `<div class='text-danger'>${results["error"]}</div>`
-    } else if("message" in results){
-      driverReportOutcome.innerHTML = `<div class="text-success">${results["message"]}</div>`
-    }
-    
-
+	try {
+		if("msg" in results){
+			driverReportOutcome.innerHTML = "<div class='text-danger fw-bold'>You must be logged in to report a pothole!</div>"
+		} else if("error" in results){
+			driverReportOutcome.innerHTML = `<div class='text-danger fw-bold'>${results["error"]}</div>`
+		} else if("message" in results){
+			driverReportOutcome.innerHTML = `<div class="text-success fw-bold">${results["message"]}</div>`
+		}
+	} catch (e) {
+		driverReportOutcome.innerHTML = `<div class="text-danger fw-bold">An unknown error has occurred.</div>`
+	}
 }
 
 
 async function postStandardReport() {
+	//STEP1: UPLOAD IMAGE
+	let imageUploadedResult = uploadImage()
 
-  //STEP1: UPLOAD IMAGE
-  let imageCheck = uploadImage()
+	if (!imageUploadedResult) {
+		//call method to upload only descripiton
+		imageUploadedResult = await makeRequest(null, description, standardReportURL)
+	}
 
-  if (imageCheck == false) {
-    //call method to upload only descripiton
-    const url = "http://spotdpothole.herokuapp.com" + "/api/reports/standard"
-    let results = makeRequest(null, url)
-  }
-  
+	let standardReportOutcome = document.querySelector("#standardReportOutcome")
 
+	try {
+		if("msg" in results){
+			standardReportOutcome.innerHTML = "<div class='text-danger fw-bold text-center'>You must be logged in to report a pothole!</div>"
+		} else if("error" in results){
+			standardReportOutcome.innerHTML = `<div class='text-danger fw-bold text-center'>${results["error"]}</div>`
+		} else if("message" in results){
+			standardReportOutcome.innerHTML = `<div class="text-success fw-bold text-center">${results["message"]}</div>`
+		}
+	} catch (e) {
+		standardReportOutcome.innerHTML = `<div class="text-danger fw-bold text-center">An unknown error has occurred.</div>`
+	}
 }
 
 
-async function makeRequest(photoURL = null, url) {
-
-  var latitude;
-  var longitude;
-  //STEP2: get location
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      //Successful action
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
+async function makeRequest(photoURL = null, description, url) {
+	var latitude, longitude;
+	//STEP2: get location
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			//Successful action
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
 
 
-    }, alert("To obtain your location, permission must be granted first."))
+			//Insert latitude/longitude error check here.
+			if(longitude == null || latitude == null){
+				//add display message here
+				displayToast("failed", "unfortunately we couldn't find your coordinates!")
+				return;
+			}
 
-    
-
-    let description = document.getElementById("descriptionText").value; // get text
-
-    const data = {
-      "longitude": longitude,
-      "latitude": latitude,
-      "description": description,
-      "images": []
-    };
-
-    if( photoURL != null){
-      data["images"] = [photoURL];
-    }
-      
-
-    console.log( JSON.stringify(data) );
-
-    try {
-      let access_token = window.localStorage.getItem("access_token");
-     
-
-      let request = {
-        "method": "POST",
-        "headers": {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${access_token}`,
-          
-        },
-        "body":JSON.stringify(data)
-      }
-      
+			buildReportRequest(latitude, longitude, photoURL, description, url)
+		}, alert("To obtain your location, permission must be granted first."))
+ 	}
+}
 
 
-      
-      let response = await fetch(url, request);
-      let results = await response.json()
 
-      //console.log("Post Request Results: " + JSON.stringify(results) ); //3. Do something with the message
-      if( "message" in results && ("Successfully" in results["message"] || "updated" in results["message"] )){
-        //add display message here
-        displayToast("success", results["message"])
-      }
-      else if(results) {
-        displayToast("failed", results["message"])
-      }
-    }
-    catch (e){
-      console.log(e)
-      return {"error" : "An unexpected error has occurred!"};
-  }
+async function buildReportRequest(latitude, longitude, photoURL, description = null, url){
+	console.log(latitude, longitude)
+  
+	var data = {
+		"longitude": longitude,
+		"latitude": latitude,
+	};
 
-  }//end of if
+	if(description != null){
+		data["description"] = description
+	}
 
+	if(photoURL != null){
+		data["images"] = [photoURL];
+	} else {
+		data["images"] = []
+	}
+		
+	console.log(JSON.stringify(data));
+  
+	try {
+		let access_token = window.localStorage.getItem("access_token");
+	   
+		let request = {
+			"method": "POST",
+			"headers": {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${access_token}`,
+			},
+			"body":JSON.stringify(data)
+		}
+		
+		let response = await fetch(url, request);
+		let results = await response.json()
+  
+		console.log("Post Request Results: " + JSON.stringify(results) ); //3. Do something with the message
+
+	} catch (error) {
+		console.log(`Error: ` + error)
+	}
 }
 
 
 // Firebase 
-function uploadImage() {
-  console.log('Entered')
-  //const ref = myStorage.ref();
+async function uploadImage() {
+	console.log('Entered')
+	//const ref = myStorage.ref();
 
-  //Get the single file input
-  const file = document.querySelector('#photo').files[0];
+	//Get the single file input
+	const file = document.querySelector('#photo').files[0];
 
-  if (file != null) {
-    console.log(file)
+	if (file != null) {
+		console.log(file)
 
-    const fileName = new Date() + '-' + file.name
+		const fileName = "REPORT - " + new Date().toLocaleString();
 
-    //const imgRef = ref(myStorage, fileName);
-    // Create a storage reference from our storage service
-    const storageRef = ref(myStorage, fileName);
+		//const imgRef = ref(myStorage, fileName);
+		// Create a storage reference from our storage service
+		const storageRef = ref(myStorage, fileName);
 
-    const uploadTask = uploadBytesResumable(storageRef, file);
+		const uploadTask = uploadBytesResumable(storageRef, file);
 
-    console.log(uploadTask)
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed',
-      (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-        }
-      },
-      (error) => {
-        // Handle unsuccessful uploads
-      },
-      () => {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log('File available at', downloadURL);
-
-          //send the image to be backend
-          const backendurl = "http://spotdpothole.herokuapp.com" + "/api/reports/standard"
-          makeRequest(downloadURL, backendurl)
-        });
-      }
-    );
-
-    return true
-  }
-  return false
+		console.log(uploadTask)
+		// Register three observers:
+		// 1. 'state_changed' observer, called any time the state changes
+		// 2. Error observer, called on failure
+		// 3. Completion observer, called on successful completion
+		uploadTask.on('state_changed', (snapshot) => {
+			// Observe state change events such as progress, pause, and resume
+			// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			console.log('Upload is ' + progress + '% done');
+			switch (snapshot.state) {
+			case 'paused':
+				console.log('Upload is paused');
+				break;
+			case 'running':
+				console.log('Upload is running');
+				break;
+			}
+		}, (error) => {
+			// Handle unsuccessful uploads
+			console.log(error)
+			return false;
+		}, () => {
+				// Handle successful uploads on complete
+				// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					console.log('File available at', downloadURL);
+					let description = document.getElementById("descriptionText").value; // get text
+					let results = makeRequest(downloadURL, description, standardReportURL)
+					return results
+				});
+		});
+		return true
+	}
+	return false
 }
 
 document.getElementById('submit-passenger-report').addEventListener('click', postStandardReport);
