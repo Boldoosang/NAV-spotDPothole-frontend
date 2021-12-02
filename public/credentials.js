41ba646e4c0b9c97f45ada9b802a523683a582f5
Jehazekel <60390137+Jehazekel@users.noreply.github.com>
2021-12-02 02:29:41 -0400

    Update credentials.js
    added credentials for firebase app initialization

--- public/credentials.js
+++ public/credentials.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "*****",
-  authDomain: "*****",
-  projectId: "******",
-  storageBucket: "*******",
-  messagingSenderId: "*******",
-  appId: "******",
-  measurementId: "******"
+  apiKey: "AIzaSyCKX_b0xCrw1V2cBdGklcqrZubpjZmQG4s",
+  authDomain: "nullify-pothole-spotter.firebaseapp.com",
+  projectId: "nullify-pothole-spotter",
+  storageBucket: "nullify-pothole-spotter.appspot.com",
+  messagingSenderId: "109703980334",
+  appId: "1:109703980334:web:3b136c94e274e3b19386bf",
+  measurementId: "G-2NRXW14SGR"
   };
 
 export {firebaseConfig};
