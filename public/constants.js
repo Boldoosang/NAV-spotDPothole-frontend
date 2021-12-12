b3d3b208348a34895a75bd48cc51707a97eaee69
Boldoosang <jbaldeo@yahoo.com>
2021-12-12 11:46:55 -0400

    Updated year to match picong party API specs

--- public/constants.js
+++ public/constants.js
@@ -1,6 +1,6 @@
 //Sets constants for backend.
 const SERVER = "https://spotdpothole.herokuapp.com/"
 const PICONG_SERVER = "https://project-caigual.herokuapp.com/publicAPI/info/electoraldistrict"
-const ELECTION_YEAR = "2020"
+const ELECTION_YEAR = "10"
 const DRIVER_REPORT_URL = SERVER + "/api/reports/driver"
 const STANDARD_REPORT_URL = SERVER + "/api/reports/standard"
