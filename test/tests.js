cffc634183a4ab63fd035d828f2d14ce1ff84077
tevonthatcher <60511580+tevonthatcher@users.noreply.github.com>
2022-04-04 21:00:38 -0400

    ensuring tests work with an empty database

--- test/tests.js
+++ test/tests.js
@@ -82,148 +82,8 @@ context('Integration Testing', () => {
     assert(requests.includes('https://spotdpothole.herokuapp.com/login'), "Request should contain the correct URL")
   })
 
-  //Assertion Test 2: This test checks that the offcanvas opens and closes when the relevant elements are clicked
-  it('Test 2: Check That offcanvas opens and closes', async function () {
-    this.timeout(config.timeout);
-
-    await page.waitForSelector('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
-    await page.click('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
-
-    let offcanvas = await getHTML('#offcanvasReport')
-    assert(offcanvas.includes('offcanvas offcanvas-end show'), "Offcanvas should be open")
-
-    await page.waitForTimeout(500)
-
-    await page.waitForSelector('#offcanvasReport > .offcanvas-header > .btn-close')
-    await page.click('#offcanvasReport > .offcanvas-header > .btn-close')
-
-    offcanvas = await getHTML('#offcanvasReport')
-
-    assert(!offcanvas.includes('offcanvas offcanvas-end show'), "Offcanvas should be closed")
-  })
-
-  // //Assertion Test 3: Checks that the offcanvas displays the correct data.
-  it('Test 3: Check that The offcanvas contains constituency data', async function () {
-    this.timeout(config.timeout);
-
-    await page.waitForSelector('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
-    await page.click('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
-
-    await page.waitForTimeout(500)
-
-    let url = requests.slice().reverse().find(a => a.includes('https://project-caigual.herokuapp.com/'))
-
-    const response = await fetch(url);
-    const body = await response.json();
-
-    let offcanvas = await getHTML('#councillorInformation')
-
-    assert(offcanvas.includes(body[0].name), "Offcanvas should contain the name of the councillor")
-    assert(offcanvas.includes(body[0].address), "Offcanvas should contain the address of the councillor")
-    assert(offcanvas.includes(body[0].email), "Offcanvas should contain the email address of the councillor")
-    assert(offcanvas.includes(body[0].phone), "Offcanvas should contain the phone number of the councillor")
-
-    await page.waitForSelector('#offcanvasReport > .offcanvas-header > .btn-close')
-    await page.click('#offcanvasReport > .offcanvas-header > .btn-close')
-  })
-
-  //Assertion Test 4: Checks that the constituency leaderboard displays
-  it('Test 4: Check that the constituency leaderboard displays', async function () {
-    this.timeout(config.timeout);
-
-    await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
-    await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
-
-    await page.waitForSelector('#pills-constLeaderboard-tab')
-    await page.click('#pills-constLeaderboard-tab')
-
-    await page.waitForTimeout(1000)
-
-    let leaderboard = await getHTML('#constLeaderboard')
-    let result = leaderboard.includes("RANK") && leaderboard.includes("CONSTITUENCY") && leaderboard.includes("NUMBER OF POTHOLES") && leaderboard.includes("CONSTITUENCY LEADER")
-    assert(result)
-  })
-
-  // //Assertion Test 5: Checks that the report leaderboard displays
-  it('Test 5: Check that the report leaderboard displays', async function () {
-    this.timeout(config.timeout);
-
-    await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
-    await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
-
-    await page.waitForSelector('.d-flex > #navbar > .list-group > li:nth-child(5) > a')
-    await page.click('.d-flex > #navbar > .list-group > li:nth-child(5) > a')
-
-    await page.waitForSelector('#pills-reportLeaderboard-tab')
-    await page.click('#pills-reportLeaderboard-tab')
-
-    await page.waitForTimeout(1000)
-
-    let leaderboard = await getHTML('#reportLeaderboard')
-    assert(leaderboard.includes("POTHOLE ID"), "leaderboard should display an ID")
-    assert(leaderboard.includes("NUMBER OF REPORTS"), "leaderboard should display a number of reports")
-    assert(leaderboard.includes("CONSTITUENCY"), "leaderboard should display a constituency")
-  })
-
-  //Assertion Test 6: Checks that the councillor information modal loads when the relevant data is pressed
-  it('Test 6: Check that the councillor information loads when button pressed', async function () {
-    this.timeout(config.timeout);
-
-    await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
-    await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
-
-    await page.waitForSelector('#pills-constLeaderboard-tab')
-    await page.click('#pills-constLeaderboard-tab')
-
-    await page.waitForSelector('#constLeaderboard > tbody > #fstPlaceRow > td > .btn')
-    await page.click('#constLeaderboard > tbody > #fstPlaceRow > td > .btn')
-
-    await page.waitForTimeout(2000)
-
-    let url = requests.slice().reverse().find(a => a.includes('https://project-caigual.herokuapp.com/'))
-
-    const response = await fetch(url);
-
-    const body = await response.json();
-
-    let infoModal = await getHTML('#councillorInfoModal > .modal-dialog > .modal-content > .modal-body')
-
-    assert(infoModal.includes(body[0].name), "Modal should contain the name of the councillor")
-    assert(infoModal.includes(body[0].address), "Modal should contain the address of the councillor")
-    assert(infoModal.includes(body[0].email), "Modal should contain the email address of the councillor")
-    assert(infoModal.includes(body[0].phone), "Modal should contain the phone number of the councillor")
-  })
-
-  //Assertion Test 7: Checks that clicking on the pothole ID opens the correct councillor info in the offcanvas
-  it('Test 7: Check that clicking on the pothole ID opens the correct councillor info', async function () {
-    this.timeout(config.timeout);
-
-    await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
-    await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
-
-    await page.waitForSelector('#pills-reportLeaderboard-tab')
-    await page.click('#pills-reportLeaderboard-tab')
-
-    await page.waitForSelector('.card > #reportLeaderboard > tbody > #fstPlaceRow > .text-dark')
-    await page.click('.card > #reportLeaderboard > tbody > #fstPlaceRow > .text-dark')
-
-    let url = requests.slice().reverse().find(a => a.includes('https://project-caigual.herokuapp.com/'))
-
-    const response = await fetch(url);
-    const body = await response.json();
-
-    let offcanvas = await getHTML('#offcanvasReport')
-
-    assert(offcanvas.includes(body[0].name), "Offcanvas should contain the name of the councillor")
-    assert(offcanvas.includes(body[0].address), "Offcanvas should contain the address of the councillor")
-    assert(offcanvas.includes(body[0].email), "Offcanvas should contain the email address of the councillor")
-    assert(offcanvas.includes(body[0].phone), "Offcanvas should contain the phone number of the councillor")
-
-    await page.waitForSelector('#offcanvasReport > .offcanvas-header > .btn-close')
-    await page.click('#offcanvasReport > .offcanvas-header > .btn-close')
-  })
-
-  it('Test 8: Test Driver Report', async function () {
+  //Test 2: Create a driver report and test that it generates the appropriate request
+  it('Test 2: Test Driver Report', async function () {
     this.timeout(config.timeout);
 
     await page.waitForSelector('#navbar > .list-group > li:nth-child(2) > a > span')
@@ -243,6 +103,148 @@ context('Integration Testing', () => {
     assert(requests.includes("https://spotdpothole.herokuapp.com/api/reports/driver"), "A request to the driver report endpoint should have been made")
   })
 
+    //Assertion Test 3: This test checks that the offcanvas opens and closes when the relevant elements are clicked
+    it('Test 3: Check That offcanvas opens and closes', async function () {
+      this.timeout(config.timeout);
+  
+      await page.waitForSelector('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
+      await page.click('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
+  
+      let offcanvas = await getHTML('#offcanvasReport')
+      assert(offcanvas.includes('offcanvas offcanvas-end show'), "Offcanvas should be open")
+  
+      await page.waitForTimeout(500)
+  
+      await page.waitForSelector('#offcanvasReport > .offcanvas-header > .btn-close')
+      await page.click('#offcanvasReport > .offcanvas-header > .btn-close')
+  
+      offcanvas = await getHTML('#offcanvasReport')
+  
+      assert(!offcanvas.includes('offcanvas offcanvas-end show'), "Offcanvas should be closed")
+    })
+  
+    // //Assertion Test 4: Checks that the offcanvas displays the correct data.
+    it('Test 4: Check that The offcanvas contains constituency data', async function () {
+      this.timeout(config.timeout);
+  
+      await page.waitForSelector('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
+      await page.click('#mapContent > #map > .leaflet-pane > .leaflet-pane > .leaflet-marker-icon:nth-child(1)')
+  
+      await page.waitForTimeout(500)
+  
+      let url = requests.slice().reverse().find(a => a.includes('https://project-caigual.herokuapp.com/'))
+  
+      const response = await fetch(url);
+      const body = await response.json();
+  
+      let offcanvas = await getHTML('#councillorInformation')
+  
+      assert(offcanvas.includes(body[0].name), "Offcanvas should contain the name of the councillor")
+      assert(offcanvas.includes(body[0].address), "Offcanvas should contain the address of the councillor")
+      assert(offcanvas.includes(body[0].email), "Offcanvas should contain the email address of the councillor")
+      assert(offcanvas.includes(body[0].phone), "Offcanvas should contain the phone number of the councillor")
+  
+      await page.waitForSelector('#offcanvasReport > .offcanvas-header > .btn-close')
+      await page.click('#offcanvasReport > .offcanvas-header > .btn-close')
+    })
+  
+    //Assertion Test 5: Checks that the constituency leaderboard displays
+    it('Test 5: Check that the constituency leaderboard displays', async function () {
+      this.timeout(config.timeout);
+  
+      await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
+      await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
+  
+      await page.waitForSelector('#pills-constLeaderboard-tab')
+      await page.click('#pills-constLeaderboard-tab')
+  
+      await page.waitForTimeout(1000)
+  
+      let leaderboard = await getHTML('#constLeaderboard')
+      let result = leaderboard.includes("RANK") && leaderboard.includes("CONSTITUENCY") && leaderboard.includes("NUMBER OF POTHOLES") && leaderboard.includes("CONSTITUENCY LEADER")
+      assert(result)
+    })
+  
+    // //Assertion Test 6: Checks that the report leaderboard displays
+    it('Test 6: Check that the report leaderboard displays', async function () {
+      this.timeout(config.timeout);
+  
+      await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
+      await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
+  
+      await page.waitForSelector('.d-flex > #navbar > .list-group > li:nth-child(5) > a')
+      await page.click('.d-flex > #navbar > .list-group > li:nth-child(5) > a')
+  
+      await page.waitForSelector('#pills-reportLeaderboard-tab')
+      await page.click('#pills-reportLeaderboard-tab')
+  
+      await page.waitForTimeout(1000)
+  
+      let leaderboard = await getHTML('#reportLeaderboard')
+      assert(leaderboard.includes("POTHOLE ID"), "leaderboard should display an ID")
+      assert(leaderboard.includes("NUMBER OF REPORTS"), "leaderboard should display a number of reports")
+      assert(leaderboard.includes("CONSTITUENCY"), "leaderboard should display a constituency")
+    })
+  
+    //Assertion Test 7: Checks that the councillor information modal loads when the relevant data is pressed
+    it('Test 7: Check that the councillor information loads when button pressed', async function () {
+      this.timeout(config.timeout);
+  
+      await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
+      await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
+  
+      await page.waitForSelector('#pills-constLeaderboard-tab')
+      await page.click('#pills-constLeaderboard-tab')
+  
+      await page.waitForSelector('#constLeaderboard > tbody > #fstPlaceRow > td > .btn')
+      await page.click('#constLeaderboard > tbody > #fstPlaceRow > td > .btn')
+  
+      await page.waitForTimeout(2000)
+  
+      let url = requests.slice().reverse().find(a => a.includes('https://project-caigual.herokuapp.com/'))
+  
+      const response = await fetch(url);
+  
+      const body = await response.json();
+  
+      let infoModal = await getHTML('#councillorInfoModal > .modal-dialog > .modal-content > .modal-body')
+  
+      assert(infoModal.includes(body[0].name), "Modal should contain the name of the councillor")
+      assert(infoModal.includes(body[0].address), "Modal should contain the address of the councillor")
+      assert(infoModal.includes(body[0].email), "Modal should contain the email address of the councillor")
+      assert(infoModal.includes(body[0].phone), "Modal should contain the phone number of the councillor")
+    })
+  
+    //Assertion Test 8: Checks that clicking on the pothole ID opens the correct councillor info in the offcanvas
+    it('Test 8: Check that clicking on the pothole ID opens the correct councillor info', async function () {
+      this.timeout(config.timeout);
+  
+      await page.waitForSelector('#navbar > .list-group > li:nth-child(5) > a > span')
+      await page.click('#navbar > .list-group > li:nth-child(5) > a > span')
+  
+      await page.waitForSelector('#pills-reportLeaderboard-tab')
+      await page.click('#pills-reportLeaderboard-tab')
+  
+      await page.waitForSelector('.card > #reportLeaderboard > tbody > #fstPlaceRow > .text-dark')
+      await page.click('.card > #reportLeaderboard > tbody > #fstPlaceRow > .text-dark')
+  
+      let url = requests.slice().reverse().find(a => a.includes('https://project-caigual.herokuapp.com/'))
+  
+      const response = await fetch(url);
+      const body = await response.json();
+  
+      let offcanvas = await getHTML('#offcanvasReport')
+  
+      assert(offcanvas.includes(body[0].name), "Offcanvas should contain the name of the councillor")
+      assert(offcanvas.includes(body[0].address), "Offcanvas should contain the address of the councillor")
+      assert(offcanvas.includes(body[0].email), "Offcanvas should contain the email address of the councillor")
+      assert(offcanvas.includes(body[0].phone), "Offcanvas should contain the phone number of the councillor")
+  
+      await page.waitForSelector('#offcanvasReport > .offcanvas-header > .btn-close')
+      await page.click('#offcanvasReport > .offcanvas-header > .btn-close')
+    })
+
+  //Test 9: Check that the dashboard modal contains the correct elements
   it('Test 9: Test that the dashboard modal displays correctly', async function () {
     this.timeout(config.timeout);
 
@@ -264,6 +266,7 @@ context('Integration Testing', () => {
     assert(potholeModal.includes("Delete"), "Modal should contain the text 'Delete'")
   })
 
+  //Test 10: Check that deleting a driver report generates the correct request
   it('Test 10: Test Delete Driver Report', async function(){
     await page.goto('https://spotdpoth.web.app/')
     this.timeout(config.timeout);
@@ -284,8 +287,11 @@ context('Integration Testing', () => {
 
     await page.waitForSelector('#pills-deleteTab > .mt-4 > .text-white >.mt-4 > .btn-danger')
     await page.click('#pills-deleteTab > .mt-4 > .text-white >.mt-4 > .btn-danger')
+    
+    assert(requests.includes("https://spotdpothole.herokuapp.com/api/potholes"), "A request to the pothole endpoint should have been made")
   })
 
+  //Test 11: Check that generating a standard report generates the correct request
   it('Test 11: Test Standard Report', async function () {
     await page.goto('https://spotdpoth.web.app/')
     this.timeout(config.timeout);
@@ -309,6 +315,7 @@ context('Integration Testing', () => {
     assert(requests.includes("https://spotdpothole.herokuapp.com/api/reports/standard"), "A request to the standard report endpoint should have been made")
   })
 
+  //Test 12: Check that deleting a standard report generates the correct request
   it('Test 12: Test Delete Standard Report', async function(){
     await page.goto('https://spotdpoth.web.app/')
     this.timeout(config.timeout);
@@ -329,6 +336,8 @@ context('Integration Testing', () => {
 
     await page.waitForSelector('#pills-deleteTab > .mt-4 > .text-white >.mt-4 > .btn-danger')
     await page.click('#pills-deleteTab > .mt-4 > .text-white >.mt-4 > .btn-danger')
+
+    assert(requests.includes("https://spotdpothole.herokuapp.com/api/potholes"), "A request to the pothole endpoint should have been made")
   })
 })
 
