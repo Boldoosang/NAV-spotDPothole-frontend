5dc06450dffa5738fcc487fc1fd6443074965304
Boldoosang <jbaldeo@yahoo.com>
2023-10-30 05:45:24 -0400

    Updated backend endpoint

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,5 +1,5 @@
 //Sets constants for backend.
-const SERVER = "https://spotdpothole-rmd8je2r.b4a.run"
+const SERVER = "https://spotdpotholebe.justinbaldeo.com"
 const DRIVER_REPORT_URL = SERVER + "/api/reports/driver"
 const STANDARD_REPORT_URL = SERVER + "/api/reports/standard"
 let DEBUG = false
